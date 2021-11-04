380f81961beeaa061044d09bb0c9fac55c73ced5
Dom B <domlamont@yahoo.com>
2021-11-04 08:30:43 -0700

    Modified Changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 //Used for the overall flow of the application.
 /////////////////////////////////////////////////////////////////
 //#region 
+
 // app is the function called to start the entire application
 function app(people){
   let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
@@ -25,19 +26,19 @@ function app(people){
   // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
   mainMenu(searchResults, people);
 }
-​
+
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-​
+
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-​
+
   if(!person){
     alert("Could not find that individual.");
     return app(people); // restart
   }
-​
+
   let displayOption = promptFor("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid);
-​
+
   switch(displayOption){
     case "info":
     // TODO: get person's info
@@ -58,19 +59,19 @@ function mainMenu(person, people){
     return mainMenu(person, people); // ask again
   }
 }
-​
+
 //#endregion
-​
+
 //Filter functions.
 //Ideally you will have a function for each trait.
 /////////////////////////////////////////////////////////////////
 //#region 
-​
+
 //nearly finished function used to search through an array of people to find matching first and last name and return a SINGLE person object.
 function searchByName(people){
   let firstName = promptFor("What is the person's first name?", autoValid);
   let lastName = promptFor("What is the person's last name?", autoValid);
-​
+
   let foundPerson = people.filter(function(potentialMatch){
     if(potentialMatch.firstName === firstName && potentialMatch.lastName === lastName){
       return true;
@@ -83,43 +84,43 @@ function searchByName(people){
   console.log(foundPerson);
   return foundPerson[0];
 }
-​
+
 //unfinished function to search through an array of people to find matching eye colors. Use searchByName as reference.
 function searchByEyeColor(people){
-​   
-}
-​
-//TODO: add other trait filter functions here.
 
+}
+
+//TODO: add other trait filter functions here.
 function searchByGender(people){
   let personsGender = promptFor("What is the person's gender?", autoValid);
   let peopleOfSameGender = people.filter(function(potentialMatch){
-  if(potentialMatch.gender === personsGender){
-    return true;
-  }else{
+    if(potentialMatch.gender === personsGender){
+      return true;
+    }
+    else{
       return false;
     }
-  });
+  })
   // TODO: find the person single person object using the name they entered.
   console.log(peopleOfSameGender);
-
   return peopleOfSameGender;
 }
 
+
 //#endregion
-​
+
 //Display functions.
 //Functions for user interface.
 /////////////////////////////////////////////////////////////////
 //#region 
-​
+
 // alerts a list of people
 function displayPeople(people){
   alert(people.map(function(person){
     return person.firstName + " " + person.lastName;
   }).join("\n"));
 }
-​
+
 function displayPerson(person){
   // print all of the information about a person:
   // height, weight, age, name, occupation, eye color.
@@ -128,7 +129,7 @@ function displayPerson(person){
   // TODO: finish getting the rest of the information to display.
   alert(personInfo);
 }
-​
+
 function info(person){
   alert(`${person.firstName} ${person.lastName}:
   ID: ${person.id},
@@ -140,16 +141,16 @@ function info(person){
   Occupation: ${person.occupation}.`);
   
 }
-​
+
 //#endregion
-​
-​
-​
+
+
+
 //Validation functions.
 //Functions to validate user input.
 /////////////////////////////////////////////////////////////////
 //#region 
-​
+
 //a function that takes in a question to prompt, and a callback function to validate the user input.
 //response: Will capture the user input.
 //isValid: Will capture the return of the validation function callback. true(the user input is valid)/false(the user input was not valid).
@@ -162,7 +163,7 @@ function promptFor(question, valid){
   } while(response === ""  ||  isValid === false)
   return response;
 }
-​
+
 // helper function/callback to pass into promptFor to validate yes/no answers.
 function yesNo(input){
   if(input.toLowerCase() == "yes" || input.toLowerCase() == "no"){
@@ -172,42 +173,17 @@ function yesNo(input){
     return false;
   }
 }
-​
+
 // helper function to pass in as default promptFor validation.
 //this will always return true for all inputs.
 function autoValid(input){
   return true; // default validation only
 }
-​
+
 //Unfinished validation function you can use for any of your custom validation callbacks.
 //can be used for things like eye color validation for example.
 function customValidation(input){
   
 }
-​
+
 //#endregion
-
-
-
-
-// ALGORITHM
-
-// - Identify what code is operational and what runs in the browser already 
-// - prompt user to search by first and last name works, it returns person as "undefined undefined"
-// - program stops (code unfinished or bugged) at prompt for person "info, parents, descendants"
-// - things to be completed:
-//   - Create re-useable function for input validation that can be used throughout project. 
-//     - define criteria for valid input for each prompt.
-// - first and last name search criteria already in place, finish code to allow user to search by 
-//   other critera.  (i.e. eye color and first, name) but up to 5).
-// - return relevant list of people as defined by search criteria, verify correct results. 
-//   - after search by 2 criteria and display results is operational, consider adding more search criteria. 
-// - after search results are displayed and verified, create/finish function to display persons clientInformation. 
-// - from displayed information, display only persons descendents if applicable.
-//   - create function (loop thru arrays?) to display identify and display descendents only. 
-// - after locating person, add option (or at end of search) to display family members( parents, spouse, siblings.)
-// - make sure program runs throughout. 
-//   - create/finish function (loop and conditionals) to display family members
-// - bonus (recursion)  find out what this is. 
-
-///////////////////////////////////// {% endcomment %}
